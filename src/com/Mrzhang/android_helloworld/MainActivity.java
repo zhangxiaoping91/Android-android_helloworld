d0507310507968d583409456e6c8088451ebb8aa
zhangxiaoping91 <zhangxiaoping91@163.com>
2015-12-26 23:16:26 +0800

    一个简单的hello world

--- src/com/Mrzhang/android_helloworld/MainActivity.java
+++ src/com/Mrzhang/android_helloworld/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +12,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		TextView tv=new TextView(this);
+		tv.setText("hello world !бнбн my name is zhang  xiao ping");
+		setContentView(tv);
 	}
 
 	@Override
